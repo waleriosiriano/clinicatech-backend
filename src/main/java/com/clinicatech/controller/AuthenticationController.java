881492d186157864aee0837c04d4ba48f504f502
Walério Siriano <waleriodev@gmail.com>
2025-05-06 12:29:32 -0300

    Correções.

--- src/main/java/com/clinicatech/controller/AuthenticationController.java
+++ src/main/java/com/clinicatech/controller/AuthenticationController.java
@@ -5,6 +5,7 @@ import com.clinicatech.repository.UsuarioRepository;
 import com.clinicatech.security.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,11 +18,14 @@ public class AuthenticationController {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody Usuario usuario) {
         Usuario user = usuarioRepository.findByUsername(usuario.getUsername());
-        
-        if (user != null && user.getPassword().equals(usuario.getPassword())) {
+
+        if (user != null && passwordEncoder.matches(usuario.getPassword(), user.getPassword())) {
             String token = jwtTokenProvider.generateToken(user.getUsername());
             return ResponseEntity.ok("Bearer " + token);
         } else {
